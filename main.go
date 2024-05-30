e27938eed9df078263ad78e01be08955b5ece165
Willie Lima <willie@willielima.dev>
2024-05-30 09:34:55 +0100

    Add version printing at the start up application

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"lxdexplorer-api/api"
 )
 
+var version string
+
 func main() {
+	// print the version
+	println("LXD Explorer API Version: " + version)
+
 	// Add API routes
 	api.StartAPI()
 }
